30ce42710056cea8525e18c7ada97316c92d53bc
protechnopolis <146369803+protechnopolis@users.noreply.github.com>
2024-10-08 16:57:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,14 @@ client.on('ready', () => {
 client.on('guildMemberAdd', async (member) => {
     const channels = config.channelIds.map(id => client.channels.cache.get(id)).filter(channel => channel != null);
     for (const channel of channels) {
-        // Construire l'URL de l'API avec le nom d'utilisateur et le nom du serveur
         const welcomeCardUrl = `https://api.aggelos-007.xyz/welcomecard?avatar=${member.user.displayAvatarURL({ format: 'png', dynamic: true })}&text1=${encodeURIComponent(member.user.username)}&text2=${encodeURIComponent(`bienvenue sur le serveur ${member.guild.name}`)}`;
         
-        // Créer un embed avec EmbedBuilder
         const embed = new Discord.EmbedBuilder()
-            .setColor('#0099ff') // Couleur de l'embed
-            .setTitle(`Bienvenue ${member.user.username}!`) // Titre de l'embed
-            .setImage(welcomeCardUrl) // Ajouter l'image de la carte de bienvenue
-            .setFooter({ text: `Nous sommes heureux de t'accueillir sur ${member.guild.name}!` }); // Pied de page
+            .setColor('#0099ff')
+            .setTitle(`Bienvenue ${member.user.username}!`)
+            .setImage(welcomeCardUrl)
+            .setFooter({ text: `Nous sommes heureux de t'accueillir sur ${member.guild.name}!` });
 
-        // Envoyer l'embed dans le canal
         await channel.send({ embeds: [embed] }).catch(() => {});
     }
 });
